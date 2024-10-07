0b428f55b6917d32ee2c9dd397037df62e501513
Evgeniia-adamova <evgeniia.adamova@fspub.unibuc.ro>
2024-10-07 18:25:06 +0300

    Update hw10.py

--- hw10.py
+++ hw10.py
@@ -30,13 +30,3 @@ class Books:
         if self.author == author_name:
             self.set_stoplist(boolean)
             print(f"Book by {self.author} has been censored.")
-
-
-book1 = Books("War and Peace", "Tolstoy", 1666, 700, [True])
-book2 = Books("House", "Ivanov", 2001, 678, [False])
-book1.get_info()
-books_list = [book1, book2]
-most_expensive = Books.find_most_expensive(books_list)
-most_expensive.get_info()
-book1.set_stoplist(False)
-book1.censor("Tolstoy", False)
